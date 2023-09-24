35873a4c436ee08910f8b012ee5a38c6d3136594
pinkxpink00 <kyperu07@gmail.com>
2023-09-24 22:29:48 +0300

    add controller

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using EFPractice.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
