d338e4507d920f43f6641a6ed3042bc5c71bab2b
pinkxpink00 <kyperu07@gmail.com>
2023-09-24 13:13:29 +0300

    first commit

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFPractice.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext>options) : base (options) 
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
