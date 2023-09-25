24be50ead0330c1a866d62b24ef9ce629dff6779
pinkxpink00 <kyperu07@gmail.com>
2023-09-25 16:19:59 +0300

    add delete method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using EFPractice.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EFPractice.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User? user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
-using Microsoft.EntityFrameworkCore;
 using EFPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
